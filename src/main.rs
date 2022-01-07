625ead70324cbefb0a92b1aab7c86c62a6ceb5de
Tarun Boddupalli <tarunbod@gmail.com>
2022-01-07 00:15:05 -0500

    First raytraced image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,80 @@
+mod math;
+
+use std::path::Path;
+use std::fs::File;
+use std::io::BufWriter;
+use math::*;
+
+fn write_png(filepath: &str, data: &[u8], width: u32, height: u32) {
+    println!("Writing PNG image {}...", filepath);
+    let path = Path::new(filepath);
+    let file = File::create(path).unwrap();
+    let ref mut w = BufWriter::new(file);
+
+    let mut encoder = png::Encoder::new(w, width, height);
+    encoder.set_color(png::ColorType::Rgb);
+    encoder.set_depth(png::BitDepth::Eight);
+    encoder.set_trns(vec!(0xFFu8, 0xFFu8, 0xFFu8, 0xFFu8));
+    encoder.set_source_gamma(png::ScaledFloat::from_scaled(45455));
+    encoder.set_source_gamma(png::ScaledFloat::new(1.0 / 2.2));
+    let source_chromaticities = png::SourceChromaticities::new(
+        (0.31270, 0.32900),
+        (0.64000, 0.33000),
+        (0.30000, 0.60000),
+        (0.15000, 0.06000)
+    );
+    encoder.set_source_chromaticities(source_chromaticities);
+    let mut writer = encoder.write_header().unwrap();
+    writer.write_image_data(&data).unwrap();
+    println!("Done.");
+}
+
+fn hit_sphere(center: Point, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin - center;
+    let a = Vec3::dot(&r.direction, &r.direction);
+    let b = 2.0 * Vec3::dot(&oc, &r.direction);
+    let c = Vec3::dot(&oc, &oc) - radius*radius;
+    let discriminant = b*b - 4.0*a*c;
+    return discriminant > 0.0;
+}
+
+fn cast_ray(r: Ray) -> Color {
+    // let unit_dir = r.direction.unit();
+    if hit_sphere(Point::new(0.0,0.0,-3.0), 0.5, &r) {
+        Color::new(1.0, 0.0, 0.0)
+    } else {
+        let t = (r.direction.y + 1.0) * 0.5;
+        (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0) 
+    }
+}
+
+fn render() {
+    let aspect_ratio: f64 = 16.0 / 9.0;
+    let image_width: usize = 480;
+    let image_height = (image_width as f64 / aspect_ratio) as usize;
+    let mut data = vec![0 as u8; image_width * image_height * 3];
+
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = Point::ORIGIN - horizontal/2.0 - vertical/2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+    for i in 0..image_height {
+        for j in 0..image_width {
+            let u = j as f64 / (image_width - 1) as f64;
+            let v = i as f64 / (image_height - 1) as f64;
+            let color = cast_ray(Ray::new(Point::ORIGIN, lower_left_corner + u * horizontal + v * vertical));
+            data[((image_height - i - 1) * image_width + j) * 3 + 0] = (255.0 * color.x) as u8;
+            data[((image_height - i - 1) * image_width + j) * 3 + 1] = (255.0 * color.y) as u8;
+            data[((image_height - i - 1) * image_width + j) * 3 + 2] = (255.0 * color.z) as u8;
+        }
+    }
+    write_png("test.png", &data, image_width as u32, image_height as u32);
+}
+
 fn main() {
-    println!("Hello, world!");
+    render();
 }
---
+++ src/math.rs
@@ -0,0 +1,165 @@
+// Math Code
+use std::ops;
+
+#[derive(Clone, Copy, Debug)]
+pub struct Vec3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64
+}
+
+impl Vec3 {
+    pub const ORIGIN: Vec3 = Vec3 { x: 0.0, y: 0.0, z: 0.0 };
+
+    pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
+        Vec3 {
+            x, y, z
+        }
+    }
+
+    pub fn origin() -> Vec3 {
+        Self::new(0.0, 0.0, 0.0)
+    }
+
+    pub fn dot(u: &Vec3, v: &Vec3) -> f64 {
+        u.x * v.x + u.y * v.y + u.z * v.z
+    }
+
+    pub fn cross(u: &Vec3, v: &Vec3) -> Vec3 {
+        Vec3 {
+            x: u.y * v.z - u.z * v.y,
+            y: u.z * v.x - u.x * v.z,
+            z: u.x * v.y - u.y * v.x
+        }
+    }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        Self::dot(self, self)
+    }
+
+    pub fn unit(self) -> Vec3 {
+        let l  = self.length();
+        self / l
+    }
+}
+
+impl ops::Add for Vec3 {
+    type Output = Vec3;
+    fn add(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z
+        }
+    }
+}
+
+impl ops::AddAssign for Vec3 {
+    fn add_assign(&mut self, rhs: Vec3) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self.z += rhs.z;
+    }
+}
+
+impl ops::Sub for Vec3 {
+    type Output = Vec3;
+    fn sub(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z
+        }
+    }
+}
+
+impl ops::SubAssign<Vec3> for Vec3 {
+    fn sub_assign(&mut self, rhs: Vec3) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+        self.z -= rhs.z;
+    }
+}
+
+impl ops::Mul for Vec3 {
+    type Output = Vec3;
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: self.z * rhs.z
+        }
+    }
+}
+
+impl ops::Mul<f64> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, rhs: f64) -> Vec3 {
+        Vec3 {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs
+        }
+    }
+}
+
+impl ops::Mul<Vec3> for f64 {
+    type Output = Vec3;
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self * rhs.x,
+            y: self * rhs.y,
+            z: self * rhs.z
+        }
+    }
+}
+
+impl ops::MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.x *= rhs;
+        self.y *= rhs;
+        self.z *= rhs;
+    }
+}
+
+impl ops::Div<f64> for Vec3 {
+    type Output = Vec3;
+    fn div(self, rhs: f64) -> Vec3 {
+        Vec3 {
+            x: self.x / rhs,
+            y: self.y / rhs,
+            z: self.z / rhs
+        }
+    }
+}
+
+impl ops::DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, rhs: f64) {
+        self.x /= rhs;
+        self.y /= rhs;
+        self.z /= rhs;
+    }
+}
+
+pub type Point = Vec3;
+pub type Color = Vec3;
+
+#[derive(Debug)]
+pub struct Ray {
+    pub origin: Point,
+    pub direction: Vec3
+}
+impl Ray {
+    pub fn new(origin: Point, direction: Vec3) -> Ray {
+        Ray {
+            origin, direction
+        }
+    }
+    pub fn at(&self, t: f64) -> Point {
+        self.origin + t * self.direction
+    }
+}
